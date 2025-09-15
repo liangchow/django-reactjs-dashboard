20aa5e923c97fd6032959eb69ec0d32dac7e9819
liangchow <liangchern@gmail.com>
2025-09-14 22:01:29 -0700

    Fix api/view.py
    permission_classes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_class = [AllowAny]
+    permission_classes = [AllowAny]
