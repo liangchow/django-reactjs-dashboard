d89271bd4a85b5a2cd3d156d7da11cb36799b1bd
liangchow <liangchern@gmail.com>
2025-09-14 21:20:21 -0700

    Fix settings.py
    Correct middlewate to middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middlewater.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
