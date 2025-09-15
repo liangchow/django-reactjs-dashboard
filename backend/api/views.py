fa884314d7e93aa8f78908fd96cc7b9817f87c27
liangchow <liangchern@gmail.com>
2025-09-14 21:24:37 -0700

    Correct UserSerializer
    Correct UserSerializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,5 +7,5 @@ from .serializers import UserSerializer
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_class = [AllowAny]
