92fbbc6d68254d01e10c7e7c785d7a2160d91a35
liangchow <liangchern@gmail.com>
2025-09-16 22:42:58 -0700

    Tryout AllowAny

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,7 @@ class CreateUserView(generics.CreateAPIView):
 
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         user = self.request.user
