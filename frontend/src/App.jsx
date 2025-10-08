453242f547da5ddeed1b1b2e96b3659d5919c15b
liangchow <liangchern@gmail.com>
2025-10-07 22:20:13 -0700

    Update App.jsx
    Routes and Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState } from 'react'
 import Register  from './pages/Register'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 function Logout(){
   localStorage.clear()
@@ -16,9 +18,19 @@ function RegisterAndLogout(){
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'
+          element={
+            <ProctectedRoute>
+              <Home />
+            </ProctectedRoute>}
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<RegisterAndLogout />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
