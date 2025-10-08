2dbe14509261acdbf588fbc291329ccd84d3da05
liangchow <liangchern@gmail.com>
2025-10-07 22:15:34 -0700

    Add RegisterAndLogout function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState } from 'react'
-
+import Register  from './pages/Register'
 
 function Logout(){
-  localStorage.clearn()
+  localStorage.clear()
   return <Navigate to='/login' />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
 }
 
 function App() {
