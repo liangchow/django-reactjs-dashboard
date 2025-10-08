99319fb933a6bf9fbd230ac647077ed5579f7c00
liangchow <liangchern@gmail.com>
2025-10-07 22:14:00 -0700

    Add Logout function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState } from 'react'
+
+
+function Logout(){
+  localStorage.clearn()
+  return <Navigate to='/login' />
+}
 
 function App() {
 
