df8e044ce3659869d731625a0dffa0ae30d55ed2
liangchow <liangchern@gmail.com>
2025-10-07 22:25:55 -0700

    Update NotFound.jsx

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for does not exist!</p>
+    </div>
   )
 }
