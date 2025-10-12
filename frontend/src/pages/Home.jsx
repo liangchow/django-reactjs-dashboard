34c4bb4938594781c1cd12563aded30229881c30
liangchow <liangchern@gmail.com>
2025-10-12 14:45:46 -0700

    Work on Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import api from '../api'
 
 export default function Home() {
+
+  const [notes, setNotes] = useState([])
+  const [content, setContent] = useState('')
+  const [title, setTitle] = useState('')
+
+  const getNote = () => {
+    api.get()
+  }
+
+
+
   return (
     <div>Home</div>
   )
