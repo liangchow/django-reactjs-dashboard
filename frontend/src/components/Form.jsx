c74896e29ff229997cd77016367fdaa0d099889e
liangchow <liangchern@gmail.com>
2025-10-13 22:21:44 -0700

    Update Form.jsx

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import api from '../api'
 import { useNavigate } from 'react-router-dom'
-import { ACCESS_TOKEN, REFRESH_TOEKEN } from '../constants'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
 
 export default function Form({route, method}) {
     const [username, setUsername] = useState('')
